1e1824a41a59b116e6f0ddf95a2870d1f67d0725
ahyalfan <alfandi0857@gmail.com>
2025-11-02 03:23:08 +0700

    chore: add internal codec for dotenv, impl interface codec

---
+++ internal/encoding/dotenv/codec.go
@@ -0,0 +1,230 @@
+// Package dotenv
+package dotenv
+
+import (
+	"bytes"
+	"log"
+	"reflect"
+	"strconv"
+	"strings"
+
+	utility "github.com/ahyalfan/gathuk/internal/utils"
+	"github.com/ahyalfan/gathuk/shared"
+)
+
+type Codec[T any] struct {
+	temp map[string][]byte
+}
+
+func (c *Codec[T]) Encode(val T) ([]byte, error) {
+	return nil, nil
+}
+
+// func (c *Codec[T]) Decode(buf []byte) (T, error) {
+// 	var (
+// 		value      T
+// 		skip, next int
+// 	)
+//
+// 	skip = -1
+//
+// 	for i, v := range buf {
+//
+// 		if v == '#' {
+// 			skip = i
+// 		}
+// 		if v == '\n' || v == '\r' || i == (len(buf)-1) {
+// 			bm := buf[next:i]
+//
+// 			if i == len(buf)-1 {
+// 				bm = buf[next : i+1]
+// 			}
+//
+// 			if skip >= 0 {
+// 				bm = buf[next:skip]
+// 			}
+//
+// 			skip = -1
+// 			next = i + 1
+//
+// 			bm = bytes.TrimSpace(bm)
+// 			fmt.Println(string(bm))
+// 			bs := bytes.Split(bm, []byte(" "))
+//
+// 			if len(bs) < 1 {
+// 				continue
+// 			}
+// 			bs = bytes.Split(bs[0], []byte("="))
+//
+// 			if len(bs) < 2 {
+// 				continue
+// 			}
+//
+// 			// c.temp[string(bs[0])] = bs[1]
+// 			fmt.Printf("key->%s,value->%s \n", string(bs[0]), string(bs[1]))
+// 		}
+// 	}
+//
+// 	return value, nil
+// }
+
+func (c *Codec[T]) Decode(buf []byte) (T, error) {
+	var value T
+
+	if c.temp == nil {
+		c.temp = make(map[string][]byte)
+	}
+
+	lines := bytes.SplitSeq(buf, []byte{'\n'})
+
+	for line := range lines {
+
+		line = bytes.TrimSpace(line)
+		escape := bytes.IndexByte(line, '#')
+		if escape != -1 {
+			line = line[:escape]
+		}
+
+		bs := bytes.Split(line, []byte(" "))
+
+		if len(bs) < 1 {
+			continue
+		}
+		bs = bytes.Split(bs[0], []byte("="))
+
+		if len(bs) < 2 {
+			continue
+		}
+
+		c.temp[string(bs[0])] = bs[1]
+	}
+
+	err := c.scanWithNestedPrefix(&value)
+
+	return value, err
+}
+
+func (c *Codec[T]) scanWithNestedPrefix(v *T) error {
+	if reflect.TypeOf(v).Kind() != reflect.Ptr {
+		panic("value is not pointer")
+	}
+
+	vt := reflect.ValueOf(v).Elem()
+	parent := reflect.TypeOf(v)
+	c.scanNestedWithNestedPrefix(parent, vt, "")
+
+	return nil
+}
+
+func (c *Codec[T]) scanNestedWithNestedPrefix(
+	parent reflect.Type, v reflect.Value, nestedPrefix string,
+) {
+	for i := 0; i < v.NumField(); i++ {
+		field := v.Field(i)
+		structField := v.Type().Field(i)
+
+		if structField.Type.Kind() == reflect.Struct && structField.Type != parent {
+			nestedName := structField.Tag.Get(string(shared.GetTagNestedName()))
+			if nestedName != "" {
+				if nestedPrefix != "" {
+					nestedName = nestedPrefix + "_" + nestedName
+				}
+			}
+			c.scanNestedWithNestedPrefix(parent, field, nestedName)
+			continue
+		}
+
+		var name string
+		name = structField.Tag.Get(string(shared.GetTagName()))
+		if name == "-" {
+			continue
+		}
+		if name == "" {
+			name = utility.PascalToUpperSnakeCase(structField.Name)
+		}
+
+		if nestedPrefix != "" {
+			sub := nestedPrefix + "_"
+			name = sub + name
+		}
+		name = strings.ToUpper(name)
+
+		val, ok := c.temp[name]
+		if !ok || !field.CanSet() {
+			continue
+		}
+
+		setValue(field, string(val))
+	}
+}
+
+func setValue(field reflect.Value, val string) {
+	if field.Kind() == reflect.Ptr {
+		if field.IsNil() {
+			field.Set(reflect.New(field.Type().Elem()))
+		}
+		field = field.Elem()
+	}
+
+	// Basic kinds
+	switch field.Kind() {
+	case reflect.String:
+		field.SetString(val)
+	case reflect.Int, reflect.Int64:
+		i64, err := strconv.ParseInt(val, 0, 64)
+		if err != nil {
+			log.Fatalf("convert string to int error: %+v", err)
+		}
+		field.SetInt(i64)
+	case reflect.Float64:
+		f64, err := strconv.ParseFloat(val, 64)
+		if err != nil {
+			log.Fatalf("convert string to float error: %+v", err)
+		}
+		field.SetFloat(f64)
+	case reflect.Bool:
+		bVal, err := strconv.ParseBool(val)
+		if err != nil {
+			log.Fatalf("convert string to bool error: %+v", err)
+		}
+		field.SetBool(bVal)
+	}
+}
+
+func setValueAny(field reflect.Value, val any) {
+	if field.Kind() == reflect.Ptr {
+		if field.IsNil() {
+			field.Set(reflect.New(field.Type().Elem()))
+		}
+		field = field.Elem()
+	}
+
+	// Basic kinds
+	switch field.Kind() {
+	case reflect.String:
+		s, ok := val.(string)
+		if !ok {
+			return
+		}
+		field.SetString(s)
+
+	case reflect.Int, reflect.Int64:
+		i64, ok := val.(int)
+		if !ok {
+			log.Fatalf("convert string to int error: %+v", ok)
+		}
+		field.SetInt(int64(i64))
+	case reflect.Float64:
+		f64, ok := val.(float64)
+		if !ok {
+			log.Fatalf("convert string to float error: %+v", ok)
+		}
+		field.SetFloat(f64)
+	case reflect.Bool:
+		bVal, ok := val.(bool)
+		if !ok {
+			log.Fatalf("convert string to bool error: %+v", ok)
+		}
+		field.SetBool(bVal)
+	}
+}
