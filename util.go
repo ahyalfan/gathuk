0ef666c22f74254edffa7f4771b771208f626a76
ahyalfan <alfandi0857@gmail.com>
2025-11-02 20:47:40 +0700

    chore: add isZeroValue function util

--- util.go
+++ util.go
@@ -1,6 +1,8 @@
 // Package gathuk
 package gathuk
 
+import "reflect"
+
 // resolveFilenames accepts a list of filenames and returns them.
 // If no filenames are provided, it returns a slice containing ".env" as the fallback.
 func resolveFilenames(filenames ...string) []string {
@@ -8,4 +10,9 @@ func resolveFilenames(filenames ...string) []string {
 		return []string{".env"}
 	}
 	return filenames
+}
+
+// isZeroValue a check zero or nil use reflect packgae
+func isZeroValue(v reflect.Value) bool {
+	return reflect.DeepEqual(v.Interface(), reflect.Zero(v.Type()).Interface())
 }
