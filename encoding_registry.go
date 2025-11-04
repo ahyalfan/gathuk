e53ecb2216cf29042573d654d2acaebc15cbe9e9
ahyalfan <alfandi0857@gmail.com>
2025-11-04 14:47:12 +0700

    chore: separate codec interface

--- encoding_registry.go
+++ encoding_registry.go
@@ -7,49 +7,24 @@ import (
 	"sync"
 
 	"github.com/ahyalfan/gathuk/internal/encoding/dotenv"
+	"github.com/ahyalfan/gathuk/option"
 )
 
-type Encoder[T any] interface {
-	Encode(T) ([]byte, error)
-}
-
-type Decoder[T any] interface {
-	Decode([]byte) (T, error)
-}
-
-type Codec[T any] interface {
-	Encoder[T]
-	Decoder[T]
-}
-
-type EncoderRegistry[T any] interface {
-	Encoder(format string) (Encoder[T], error)
-}
-
-type DecoderRegistry[T any] interface {
-	Decoder(format string) (Decoder[T], error)
-}
-
-type CodecRegistry[T any] interface {
-	DecoderRegistry[T]
-	EncoderRegistry[T]
-}
-
 type DefaultCodecRegistry[T any] struct {
-	codecs map[string]Codec[T]
+	codecs map[string]option.Codec[T]
 
-	sync.Mutex
+	mu sync.Mutex
 }
 
 func NewDefaultCodecRegister[T any]() *DefaultCodecRegistry[T] {
 	dcr := new(DefaultCodecRegistry[T])
-	dcr.codecs = make(map[string]Codec[T])
+	dcr.codecs = make(map[string]option.Codec[T])
 	return dcr
 }
 
-func (dcr *DefaultCodecRegistry[T]) RegisterCodec(format string, codec Codec[T]) {
-	dcr.Lock()
-	defer dcr.Unlock()
+func (dcr *DefaultCodecRegistry[T]) RegisterCodec(format string, codec option.Codec[T]) {
+	dcr.mu.Lock()
+	defer dcr.mu.Unlock()
 
 	if dcr.codecs == nil {
 		panic("dcr.codecs is nil: codec map must be initialized before use")
@@ -60,25 +35,26 @@ func (dcr *DefaultCodecRegistry[T]) RegisterCodec(format string, codec Codec[T])
 	dcr.codecs[format] = codec
 }
 
-func (dcr *DefaultCodecRegistry[T]) Encoder(format string) (Encoder[T], error) {
+func (dcr *DefaultCodecRegistry[T]) Encoder(format string) (option.Encoder[T], error) {
 	if v, ok := dcr.codec(format); ok {
 		return v, nil
 	}
 	return nil, errors.New("encoder not found for this format")
 }
 
-func (dcr *DefaultCodecRegistry[T]) Decoder(format string) (Decoder[T], error) {
+func (dcr *DefaultCodecRegistry[T]) Decoder(format string) (option.Decoder[T], error) {
 	if v, ok := dcr.codec(format); ok {
 		return v, nil
 	}
 	return nil, errors.New("decoder not found for this format")
 }
 
-func (dcr *DefaultCodecRegistry[T]) codec(format string) (Codec[T], bool) {
+func (dcr *DefaultCodecRegistry[T]) codec(format string) (option.Codec[T], bool) {
+	format = strings.ToLower(format)
 	if v, ok := dcr.codecs[format]; ok {
 		return v, true
 	}
-	format = strings.ToLower(format)
+
 	switch format {
 	case "env":
 		return &dotenv.Codec[T]{}, true
