3363cb9d48763bea1d2d6b443207dcd84f3efa16
ahyalfan <alfandi0857@gmail.com>
2025-11-15 17:13:02 +0700

    tests: add codec simple test

---
+++ internal/encoding/json/codec_test.go
@@ -0,0 +1,83 @@
+// Package json
+package json
+
+import (
+	"fmt"
+	"testing"
+
+	customtests "github.com/ahyalfan/gathuk/internal/utils/custom-test"
+)
+
+type MyStruct struct {
+	Name   string         `config:"name"`
+	Age    float64        `config:"age"`
+	Active bool           `config:"active"`
+	Items  []Item         `config:"items"`
+	Class  map[string]any `config:"class"`
+	Tour   Tour
+}
+
+type Item struct {
+	ID   string `config:"id"`
+	Name string `config:"name"`
+}
+
+// type Class struct {
+// 	ID   string `config:"id"`
+// 	Name string `config:"name"`
+// }
+
+type Tour struct {
+	ID   string `config:"id"`
+	Name string `config:"name"`
+	Size []any
+}
+
+func TestCodec(t *testing.T) {
+	ast := ObjectNode{
+		Value: map[string]ASTNode{
+			"name":   StringNode{"Alice"},
+			"age":    NumberNode{30},
+			"active": BooleanNode{true},
+			"items": ArrayNode{
+				Value: []ASTNode{
+					ObjectNode{Value: map[string]ASTNode{
+						"id":   StringNode{"1"},
+						"name": StringNode{"Item 1"},
+					}},
+					ObjectNode{Value: map[string]ASTNode{
+						"id":   StringNode{"2"},
+						"name": StringNode{"Item 2"},
+					}},
+				},
+			},
+			"class": ObjectNode{Value: map[string]ASTNode{
+				"id":   StringNode{"C1"},
+				"name": StringNode{"Math"},
+			}},
+			"tour": ObjectNode{Value: map[string]ASTNode{
+				"id":   StringNode{"T1"},
+				"name": StringNode{"Paris Tour"},
+				"size": ArrayNode{Value: []ASTNode{
+					NumberNode{10},
+					StringNode{"large"},
+					NumberNode{10.10},
+				}},
+			}},
+		},
+	}
+
+	cdc := Codec[MyStruct]{}
+
+	var result MyStruct
+	err := cdc.ASTToStruct(ast, &result)
+
+	customtests.OK(t, err)
+	fmt.Printf("%+v\n", result)
+
+	t.Run("encode", func(t *testing.T) {
+		b, err := cdc.Encode(result)
+		customtests.OK(t, err)
+		fmt.Println(string(b))
+	})
+}
